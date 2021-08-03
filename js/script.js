93c1389d0543d1da171e67aaf893d4ee1f4d690f
pranavkotwal <49470242+pranavkotwal@users.noreply.github.com>
2021-08-03 09:55:39 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,8 +82,9 @@ function printQuote (){
   const randomQuote = getRandomQuote()
   let html =`<p class = "quote"> ${randomQuote.quote}</p> 
   <p class= "source"> ${randomQuote.source}`
-  if(randomQuote.citation){html += `<span class = "citation">${randomQuote.citation}</span></p>`}
-  if(randomQuote.year){html += `<span class = "year">${randomQuote.year}</span></p>`}
+  if(randomQuote.citation){html += `<span class = "citation">${randomQuote.citation}</span>`}
+  if(randomQuote.year){html += `<span class = "year">${randomQuote.year}</span>`}
+  html +=`</p>`
 
   return document.getElementById('quote-box').innerHTML= html
   
